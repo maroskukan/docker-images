045ae05e683f88cc486c7d53fe36b7db1ec179d8
Maros Kukan <maros.kukan@me.com>
2021-05-17 15:40:37 +0200

    Add route for /books POST

--- Dockerfiles/docker-api_service/src/app.py
+++ Dockerfiles/docker-api_service/src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+import json
+from types import MethodType
+from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
@@ -15,9 +17,45 @@ books = [
     }
 ]
 
+def validBookObject(bookObject):
+    if ("name" in bookObject and
+        "price" in bookObject and
+        "isbn" in bookObject):
+        return True
+    else:
+        return False
+
+
 @app.route('/books')
 def get_books():
     return jsonify({'books': books})
+
+
+@app.route('/books', methods = ['POST'])
+def add_book():
+    request_data = request.get_json()
+    if(validBookObject(request_data)):
+        new_book = {
+            "name": request_data['name'],
+            "price": request_data['price'],
+            "isbn": request_data['isbn']
+        }
+        books.insert(0, new_book)
+        response = Response("",
+                            status=201,
+                            mimetype='application/json')
+        response.headers['Location'] = "/books/" + str(new_book['isbn'])
+    else:
+        invalidBookObjectErrorMsg = {
+            "error": "Invalid book object passwd in request",
+            "helpString": "Data passed in similar to this {'name': 'bookname', 'price': 7.99, 'isbn': 978039480016"
+        }
+        response = Response(json.dumps(invalidBookObjectErrorMsg),
+                            status=400,
+                            mimetype='application/json')
+    return response
+
+
 
 @app.route('/books/<int:isbn>')
 def get_book_by_isbn(isbn):
