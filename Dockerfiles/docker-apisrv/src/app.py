57419be598ac56665d9f14b26dd831eee6edc56c
Maros Kukan <maros.kukan@me.com>
2021-05-17 12:40:06 +0200

    Add /books and /books/<int:isbn> routes

---
+++ Dockerfiles/docker-apisrv/src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+books = [
+    {
+        'name': 'Green Eggs and Ham',
+        'price': 7.99,
+        'isbn': 978039400165
+    },
+    {
+        'name': 'The Cat In The Hat',
+        'price': 6.99,
+        'isbn': 9782371000193
+    }
+]
+
+@app.route('/books')
+def get_books():
+    return jsonify({'books': books})
+
+@app.route('/books/<int:isbn>')
+def get_book_by_isbn(isbn):
+    return_value = {}
+    for book in books:
+        if book["isbn"] == isbn:
+            return_value = {
+                'name': book['name'],
+                'price': book['price']
+            }
+    return jsonify(return_value)
+
+app.run(port=5000)
