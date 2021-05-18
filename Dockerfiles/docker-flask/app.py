3700a58acbde6108bd9647ce416d3dd8f0d7784f
Maros Kukan <maros.kukan@me.com>
2021-05-18 08:28:51 +0200

    Add shebang and PORT env var

--- Dockerfiles/docker-flask/app.py
+++ Dockerfiles/docker-flask/app.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +11,5 @@ def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
