8e5a1a63bc09c78e20d0a9d3834c8106253a8fea
Maros Kukan <maros.kukan@me.com>
2021-05-18 09:11:34 +0200

    Add modularity and servicing system

--- Dockerfiles/docker-flask/src/app.py
+++ Dockerfiles/docker-flask/src/app.py
@@ -4,11 +4,17 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    message = "Welcome to Flask Web App!"
+    return render_template('index.html',
+                            message=message,
+                            hostName=os.uname()[1])
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
