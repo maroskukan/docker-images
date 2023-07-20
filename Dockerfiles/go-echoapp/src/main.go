6045bf86f902807fd269c2ad1954c293b1f7d8c3
Maros Kukan <maros.kukan@me.com>
2023-07-20 18:30:46 +0200

    feat: return container id

--- Dockerfiles/go-echoapp/src/main.go
+++ Dockerfiles/go-echoapp/src/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Get the container ID (hostname)
+	hostname, err := os.Hostname()
+	if err != nil {
+		http.Error(w, "Error getting hostname", http.StatusInternalServerError)
+		return
+	}
+
+	// Return the response
+	fmt.Fprintf(w, "Hellow from Container ID: %s", hostname)
+}
+
 func main() {
+	http.HandleFunc("/", handler)
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-    })
-
-    log.Fatal(http.ListenAndServe(":80", nil))
-
+	// Listen on port 8080
+	fmt.Println("Starting server on port 80...")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
